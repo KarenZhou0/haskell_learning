a8285eee5ba98009f3987a5b44c4f6a597170b90
Karen Zhou <zz2202@nyu.edu>
2020-05-20 21:51:34 -0400

    chapter 9 change

--- chapter9/zzipWithAndZzip.hs
+++ chapter9/zzipWithAndZzip.hs
@@ -1,12 +1,10 @@
 zzipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-zzipWith _ [] [] = []
 zzipWith _ _ [] = []
 zzipWith _ [] _ = []
 zzipWith f (x:xs) (y:ys) = (f x y): (zzipWith f xs ys)
 
 
 zzip :: [a] -> [b] -> [(a,b)]
-zzip [] [] = []
 zzip _ [] = []
 zzip [] _ = []
 zzip (x:xs) (y:ys) = (x,y): (zzip xs ys)
